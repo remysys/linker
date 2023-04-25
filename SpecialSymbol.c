e19dd17f537f3f58af42d82356feb7bdba5d6f85
remysys <109655176+remysys@users.noreply.github.com>
2023-04-26 00:43:57 +0800

    fix(linker): fix SpecialSymbol.c

--- SpecialSymbol.c
+++ SpecialSymbol.c
@@ -1,14 +1,16 @@
+
 #include <stdio.h>
 
-extern char *__executable_start;
-extern char *etext, *_etext, *__etext;
-extern char *edata, *_edata;
-extern char *end, *_end;
+extern char __executable_start[];
+extern char etext[], _etext[], __etext[];
+extern char edata[], _edata[];
+extern char end[], _end[];
 
 int main() {
-  printf("Executable start %X\n", __executable_start);
-  printf("Text end %X %X %X\n", etext, _etext, __etext);
-  printf("Data end %X %X\n", edata, _edata);
-  printf("Executable end %X %X\n", end, _end);
+  printf("Executable Start %p\n", __executable_start);
+  printf("Text End %p %p %p\n", etext, _etext, __etext);
+  printf("Data End %p %p\n", edata, _edata);
+  printf("Executable End %p %p\n", end, _end);
+
   return 0;
 }
