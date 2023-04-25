cdd28efb496a1681ab961a39e666e4002120aa9c
remysys <109655176+remysys@users.noreply.github.com>
2023-04-26 01:36:15 +0800

    feat(linker): weak ref

---
+++ pthread.c
@@ -0,0 +1,13 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int pthread_create(pthread_t *, const pthread_attr_t *, void *(*)(void *), void *) __attribute__((weak));
+
+int main() {
+  if (pthread_create) {
+    puts("This is multi-threaded version.");
+  } else {
+    puts("This is single-threaded version.");
+  }
+  return 0;
+}
