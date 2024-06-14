f9c11584e8adf228e3070f924d8b5711ae191091
remysys <109655176+remysys@users.noreply.github.com>
2024-06-15 02:50:31 +0800

    feat(linker): process stack

---
+++ proc_stack.c
@@ -0,0 +1,40 @@
+/* 
+ * gcc proc_stack.c -m32 -o proc_stack
+ * ./proc_stack 1 2 3 4
+ */
+
+#include <stdio.h>
+#include <elf.h>
+
+int main(int argc, char* argv[])
+{
+  int *p = (int *)argv;
+  int i;
+  Elf32_auxv_t* aux;
+
+  printf("argument count: %d\n", *(p - 1));
+
+  for(i = 0; i < *(p - 1); i++) {
+    printf("argument %d : %s\n", i, *(p + i));
+  }
+
+  p += i;
+  p++; //skip 0
+
+  printf("environment:\n");
+  while(*p) {
+    printf("%s\n", *p);
+    p++;
+  }
+
+  p++; //skip 0
+
+  printf("auxiliary vectors:\n");
+  aux = (Elf32_auxv_t *)p;
+  while(aux->a_type != AT_NULL){
+    printf("Type: %02d Value: %x\n", aux->a_type, aux->a_un.a_val);
+    aux++;
+  }
+    
+  return 0;
+}
