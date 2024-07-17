4b0bbc82d273ba1978dd509c0e48b669684834bb
remysys <109655176+remysys@users.noreply.github.com>
2024-07-17 23:44:21 +0800

    feat(csapp) byte representation

---
+++ csapp/data/show-bytes.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+  size_t i;
+  for (i = 0; i < len; i++)
+    printf(" %.2x", start[i]);
+  printf("\n");
+}
+
+void show_int(int x) {
+  show_bytes((byte_pointer) &x, sizeof(int)); 
+}
+
+void show_float(float x) {
+  show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+  show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+
+void test_show_bytes(int val) {
+  int ival = val;
+  float fval = (float) ival;
+  int *pval = &ival;
+  show_int(ival);
+  show_float(fval);
+  show_pointer(pval);
+}
+
+
+int main(int argc, char *argv[]) {
+  int val = 12345;
+
+  if (argc > 1) {
+    val = strtol(argv[1], NULL, 0);
+    printf("calling test_show_bytes\n");
+    test_show_bytes(val);
+  }
+  return 0;
+}
